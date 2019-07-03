bbdba763baaf7ad2f9032d6ade0beac6d1dd1132
ldx <ldxing52mn@163.com>
2019-07-03 13:52:50 +0800

    add notification

--- Example/Pods/Target Support Files/PodExplainFiles/PodExplainFiles-umbrella.h
+++ Example/Pods/Target Support Files/PodExplainFiles/PodExplainFiles-umbrella.h
@@ -10,6 +10,16 @@
 #endif
 #endif
 
+#import "YQLocalNotificationModel.h"
+#import "YQMockNotificationManager.h"
+#import "YQNotificationCenterModel.h"
+#import "YQNotificationManager.h"
+#import "YQNotificationModel.h"
+#import "YQNotificationRegisterProtocol.h"
+#import "YQRegisterLocalNotificationModel.h"
+#import "YQUINotificationRegister.h"
+#import "YQUNNotificationRegister.h"
+#import "NSDate+Compare.h"
 #import "YQDeviceManager.h"
 #import "YQWeakProxy.h"
 
